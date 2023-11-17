e9df0178f01192288bd3140f257535f9bea3dd87
Hsiao Yao <tomtogo1260@gmail.com>
2023-11-18 07:18:04 +0800

    feat: add merge obj

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import open3d as o3d
+from PIL import Image
+import json
 
 def parse_obj(filename):
     vertices = []
@@ -138,10 +140,12 @@ def cut_obj(OBJ_SIMPLIFY, X_CENTER, OBJ_SIMPLIFY_LEFT, OBJ_SIMPLIFY_RIGHT):
     # make vertices align with the cutting edge (left & right triangle)
     right_edge_faces = data['faces'][tri_left_num == 1][:,:,0] - 1
     mask = data['vertices'][right_edge_faces, 0] < centers[tri_left_num == 1]
+    mask[:, 0] = np.logical_or(mask[:, 0], np.logical_not(mask.any(axis=1))) # exclude [False, False, False]
     data['vertices'][right_edge_faces[mask], 0] = centers[tri_left_num == 1, np.argmax(mask, axis=1)]
 
     left_edge_faces = data['faces'][tri_left_num == 2][:,:,0] - 1
     mask = data['vertices'][left_edge_faces, 0] > centers[tri_left_num == 2]
+    mask[:, 0] = np.logical_or(mask[:, 0], np.logical_not(mask.any(axis=1))) # exclude [False, False, False]
     data['vertices'][left_edge_faces[mask], 0] = centers[tri_left_num == 2, np.argmax(mask, axis=1)]
 
     # receive left & right surface
@@ -167,8 +171,10 @@ def cluster_obj(filename, SEGMENT_ID, SIMPLIFY, REVERSE, LEFT):
     for i in range(len(cluster_n_triangles)):
         cluster_data = data.copy()
         cluster_data['faces'] = data['faces'][triangle_clusters == i]
-        re_index(cluster_data)
 
+        if(cluster_data['faces'].shape[0] < 100): continue
+
+        re_index(cluster_data)
         cluster_data_list.append(cluster_data)
         cluster_uv_list.append(np.mean(cluster_data['uvs'], axis=0))
 
@@ -180,9 +186,50 @@ def cluster_obj(filename, SEGMENT_ID, SIMPLIFY, REVERSE, LEFT):
         filename = f'{SEGMENT_ID}_s{SIMPLIFY}_{side}{sorted_id[i]}.obj'
         save_obj(filename, cluster_data)
 
-SIMPLIFY = 10
+def simplify_image(input_image, output_image, factor):
+    Image.MAX_IMAGE_PIXELS = None
+    img = Image.open(input_image)
+
+    width, height = img.size
+    new_size = (width // factor, height // factor)
+    resized_img = img.resize(new_size)
+    resized_img.save(output_image)
+    img.close()
+
+def re_index_merge(data):
+    data['faces'] -= 1
+    selected_vertices = np.unique(data['faces'][:,:,0])
+
+    # only leave the vertices used to form the faces
+    data['vertices'] = data['vertices'][selected_vertices]
+    data['normals']  = data['normals'][selected_vertices]
+    data['uvs']      = data['uvs'][selected_vertices]
+    data['colors']   = data['colors'][selected_vertices]
+
+    # update face index
+    vertex_mapping = { old_index: new_index for new_index, old_index in enumerate(selected_vertices) }
+    data['faces'] = np.vectorize(lambda x: vertex_mapping.get(x, x))(data['faces'])
+    data['faces'] += 1
+
+def merge_obj(input_list, output_filename):
+    filename = input_list[0]
+    merge_data = parse_obj(filename)
+
+    for filename in input_list[1:]:
+        data = parse_obj(filename)
+        data['faces'] += merge_data['vertices'].shape[0]
+
+        merge_data['vertices'] = np.vstack((merge_data['vertices'], data['vertices']))
+        merge_data['normals'] = np.vstack((merge_data['normals'], data['normals']))
+        merge_data['uvs'] = np.vstack((merge_data['uvs'], data['uvs']))
+        merge_data['colors'] = np.vstack((merge_data['colors'], data['colors']))
+        merge_data['faces'] = np.vstack((merge_data['faces'], data['faces']))
+
+    save_obj(output_filename, merge_data)
+
+SIMPLIFY = 100
 REVERSE = False
-SEGMENT_ID = '20230510153006'
+SEGMENT_ID = '20230702185753'
 CENTER_INPUT = 'scroll1_center.obj'
 
 OBJ_INPUT = f'{SEGMENT_ID}.obj'
@@ -204,6 +251,12 @@ print("clustering ...")
 cluster_obj(OBJ_SIMPLIFY_LEFT, SEGMENT_ID, SIMPLIFY, REVERSE, True)
 cluster_obj(OBJ_SIMPLIFY_RIGHT, SEGMENT_ID, SIMPLIFY, REVERSE, False)
 
+# simplify image
+# simplify_image('20230702185753.tif', '20230702185753_s100.tif', 10)
+
+# merge .obj files
+# input_list = ['20230702185753_s100_l1.obj', '20230702185753_s100_l4.obj']
+# merge_obj(input_list, 'merge.obj')
 
 
 
